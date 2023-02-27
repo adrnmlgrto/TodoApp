2ac11075e10b942af6f3b89c1cdb26c7bfe72130
Adrian Melegrito <adrn.mlgrto@gmail.com>
2023-02-27 14:25:45 +0800

    Added form functionality with Django

--- todos/urls.py
+++ todos/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('create/', views.add_task, name='add_task'),
+    path('create/submit/', views.submit_task, name='submit_task'),
+    path('edit/<int:task_id>/', views.edit_task, name='edit_task'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods
+from django.utils import timezone
+from .models import Task
 # from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'todos/tasks.html')
+    tasks = Task.objects.all()
+    return render(request, 'todos/index.html', {'tasks_list':tasks})
+
+def add_task(request):
+    return render(request, 'todos/add_task.html')
+
+@require_http_methods(['POST'])
+def submit_task(request):
+    task_title = request.POST['task_title']
+    task_description = request.POST['task_description']
+    category = request.POST['category']
+    created_at = timezone.now()
+    task = Task(task_title=task_title, task_description=task_description, category=category, created_at=created_at)
+    task.save()
+    return redirect('index')
+
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'todos/edit_task.html', {'task': task})
